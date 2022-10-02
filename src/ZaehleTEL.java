7223f52f34f5f9963910c93c15dafacd43266057
Nomin Orgil <tuerkiiis@surfer-172-29-5-73-server.frederix-hotspot.de>
2022-10-02 13:59:27 +0200

    first initialisation

---
+++ src/ZaehleTEL.java
@@ -0,0 +1,36 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ZaehleTEL {
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Bitte geben Sie eine Zeichenfolge ein: ");
+		String eingabe = scanner.nextLine();
+		char[] zeichen = eingabe.toCharArray();
+		countTEL(zeichen);
+		scanner.close();
+
+	}
+
+	public static void countTEL(char[] zeichen) {
+		int tAnzahl = 0;
+		int eAnzahl = 0;
+		int lAnzahl = 0;
+
+		for (int i = 0; i<zeichen.length; i++){
+			if (zeichen[i] == 't' || zeichen[i] == 'T') {
+				tAnzahl++;
+			}
+			if (zeichen[i] == 'e' || zeichen[i] == 'E') {
+				eAnzahl++;
+			}
+			if (zeichen[i] == 'l' || zeichen[i] == 'L') {
+				lAnzahl++;
+			}
+
+		}
+		System.out.println("Der Buchstabe ’t’ kommt " + tAnzahl + " mal vor.");
+		System.out.println("Der Buchstabe ’e’ kommt " + eAnzahl + " mal vor.");
+		System.out.println("Der Buchstabe ’l’ kommt " + lAnzahl + " mal vor.");
+	}
+}
