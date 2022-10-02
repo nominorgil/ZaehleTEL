cc5aa61a3ff2c2201f4fb66cced013c2b58d4862
NTUMURB <njn_77@hotmail.de>
2022-10-02 14:24:38 +0200

    Unused class deleted

--- src/SimpleForLoop.java
+++
@@ -1,8 +0,0 @@
-public class SimpleForLoop {
-    public static void main(String args[]) {
-        for (int i = 10; i >= 0; i--) {
-            System.out.println(i);
-        }
-        System.out.println("START!");
-    }
-}
